2f3b82b8ebd12ffd73de183259db203d27403ea1
AbhayVivek <143114904+AbhayVivek@users.noreply.github.com>
2024-08-07 18:02:49 +0530

    upd

--- src/useAuth.js
+++ src/useAuth.js
@@ -3,7 +3,7 @@
 import { useState, useEffect, useRef, useCallback } from "react";
 import Keycloak from "keycloak-js";
 
-var KEYCLOAK_URL = "http://10.3.0.12";
+var KEYCLOAK_URL = "http://10.3.0.16";
 var REACT_URL = "http://localhost:3000";
 
 // var KEYCLOAK_URL = "http://localhost:8080";
